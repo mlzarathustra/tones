2c1bcd59ab6cf794a59b87d7265e906b52f9c4c2
miles zarathustra <mlzarathustra@gmail.com>
2016-12-09 15:46:25 -0800

    verifying checkin

--- app/src/test/java/net/miles_beyond/tones/EnvGenUnitTest.java
+++ app/src/test/java/net/miles_beyond/tones/EnvGenUnitTest.java
@@ -1,17 +1,63 @@
 package net.miles_beyond.tones;
 
+import net.miles_beyond.tones.synth.EnvGen;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ *  displays only - doesn't assert
+ *  todo - set up assertions from observed behavior
  */
 public class EnvGenUnitTest {
+
+    void show(double level) {  // level is from 0 to 1
+        int stars =(int)( 80 * level );
+        for (int i=0; i<stars; ++i) System.out.print("*");
+        System.out.println("");
+    }
+
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void envGenWorks() throws Exception {
+
+        EnvGen eg=new EnvGen("test");
+        for (int i=0; i<10; ++i) show(eg.nextVal());
+        // should be 0 before noteON
+        assertEquals(0, eg.nextVal(), 0);
+
+        eg.noteON();
+        for (int i=0; i<10; ++i) show(eg.nextVal());
+        assertEquals(0.5, eg.nextVal(), 0);
+        eg.noteOFF();
+        for (int i=0; i<30; ++i) show(eg.nextVal());
+        assertEquals(0, eg.nextVal(), 0);
+
+
+        eg=new EnvGen("test1");
+        eg.noteON();
+        for (int i=0; i<10; ++i) show(eg.nextVal());
+        eg.noteOFF();
+        for (int i=0; i<30; ++i) show(eg.nextVal());
+
+        eg=new EnvGen("test1");
+        eg.noteON();
+        for (int i=0; i<10; ++i) show(eg.nextVal());
+        eg.noteOFF();
+        for (int i=0; i<10; ++i) show(eg.nextVal());
+        eg.noteON();
+        for (int i=0; i<30; ++i) show(eg.nextVal());
+        eg.noteOFF();
+        for (int i=0; i<30; ++i) show(eg.nextVal());
+
+
+        eg.noteON();
+        for (int i=0; i<75; ++i) show(eg.nextVal());
+        assertEquals(0.75, eg.nextVal(), 0);
+        eg.noteOFF();
+        for (int i=0; i<30; ++i) show(eg.nextVal());
+        assertEquals(0, eg.nextVal(), 0);
     }
 }
