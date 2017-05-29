540044961dedc0bf26665cfb55fbb92e5110463f
miles zarathustra <mlzarathustra@gmail.com>
2017-05-29 11:06:40 -0700

    reversed note direction

---
+++ notes/NoteObjectsRev.groovy
@@ -0,0 +1,26 @@
+
+keys='+ ++ + ++ + + ++'
+letters=[*'a'..'g']*.toUpperCase()
+letters.addAll( letters )
+
+//letters = letters[2..-1] + letters[0..<2] // C - B
+
+key=0
+halfStep=0
+
+list = keys.collect {
+   
+   if (it == '+') names=[letters[key++]]
+   else names=[letters[key-1]+'#',letters[key%7]+'\\u266D'] // 266D=flat
+   
+   flatName=names[-1]
+   sharpName=names[0]
+
+   freq = 2 ** (halfStep / 12) 
+   // multiplier for a base frequency (e.g. 440)
+   //println names+'; freq='+freq
+   
+   halfStep++
+   "    new Note(\"$flatName\",\"$sharpName\",$freq, ${it=='+'})"
+
+}.reverse().join(',\n')
